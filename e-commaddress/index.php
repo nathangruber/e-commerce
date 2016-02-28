0a9a9722f84c78d16e6078e7712fc012c9f27879
nathangruber <grubernp@gmail.com>
2016-02-28 07:34:06 -0600

    Added jquery and cleaned up footer/navbar

--- e-commaddress/index.php
+++ e-commaddress/index.php
@@ -2,7 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </head>
  
 <body>
@@ -30,8 +35,9 @@
                   </thead>
                   <tbody>
                   <?php
+                   require_once '../database.php';
+                   require_once '../footer.php';
                    require_once '../navbar.php';
-                   require '../database.php';
                    $pdo = Database::connect();
                    $sql = 'SELECT * FROM address ORDER BY id DESC';
                    foreach ($pdo->query($sql) as $row) {
@@ -60,6 +66,3 @@
     </div> <!-- /container -->
   </body>
 </html>
-<?php
-require_once '../footer.php';
-?>
--- e-commaddress/update.php
+++ e-commaddress/update.php
@@ -1,7 +1,7 @@
 <?php
+    require_once '../database.php';
+    require_once '../footer.php';
     require_once '../navbar.php';
-    require '../database.php';
- 
     if ( !isset($_GET['id']) || empty($_GET['id'])) {
         header("Location: index.php");
     } 
@@ -96,7 +96,12 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </head>
  
 <body>
@@ -181,6 +186,3 @@
                  
     </div> <!-- /container -->
   </body>
-<?php
-require_once '../footer.php';
-?>
