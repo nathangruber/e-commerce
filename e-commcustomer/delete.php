bcdb870826470fd07d7146c3b7b6e1c21d5db5ef
nathangruber <grubernp@gmail.com>
2016-02-29 18:53:53 -0600

    Added j-query-bootstrap info

--- e-commcustomer/delete.php
+++ e-commcustomer/delete.php
@@ -29,6 +29,10 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">  
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </head>
  
 <body>
