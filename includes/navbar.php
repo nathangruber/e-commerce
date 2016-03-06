a85e1dcf9bc4b3be00041c2fd840e70e8e4dde03
nathangruber <grubernp@gmail.com>
2016-03-06 13:08:51 -0600

    navbar edit

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,7 +7,7 @@
          <span class="icon-bar"></span>
          <span class="icon-bar"></span>
        </button>
-       <a class="navbar-brand" href="index.php" id="black"><img class="img-responsive logo" src="assets/img/bbs.png" height="12" width="32"></a>
+       <a class="navbar-brand" href="index.php" id="black"><img class="img-responsive logo" src="assets/img/bbs.png" height="12" width="22"></a>
      </div>
      <div id="navbar" class="collapse navbar-collapse">
        <ul class="nav navbar-nav">
