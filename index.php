0a72d9f937f0f6d3aafb82de1d37cc8d0d247635
nathangruber <grubernp@gmail.com>
2016-03-06 13:16:13 -0600

    edited columns in index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </div>
             <div class="container">
            <div class = "row">
-         <div class="col-md-6s">
+         <div class="col-md-6">
       <div class = "home">       
 <p>Blake’s is a locally-owned skateboard shop.</p><br>
      </div>
@@ -19,7 +19,7 @@
       <div class="container" >
       <div class="starter-template">
         <h1></h1>
-        <p class="lead"><small>Please Login or click Register to set up an account with us.<br></p>
+        <p class="lead"><small>Please Login or click Register to set up an account with Blake's.<br></p>
       </div>
       <div>
         <center>
