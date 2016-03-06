c466ae19742070e63c8af0c2041c3986a2af9f97
nathangruber <grubernp@gmail.com>
2016-03-06 13:11:22 -0600

    put img in div, in navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,8 +7,9 @@
          <span class="icon-bar"></span>
          <span class="icon-bar"></span>
        </button>
+       <div>
        <a class="navbar-brand" href="index.php" id="black"><img class="img-responsive logo" src="assets/img/bbs.png" height="12" width="27"></a>
-     <a class="navbar-brand" href="index.php" id="black"><img class="img-responsive logo" src="assets/img/bbs.png" height="12" width="27"></a>
+      </di>v
      </div>
      <div id="navbar" class="collapse navbar-collapse">
        <ul class="nav navbar-nav">
