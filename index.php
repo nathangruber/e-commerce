7378e2dbbf6ae5b16c2cb98b0f33088afe914de9
nathangruber <grubernp@gmail.com>
2016-03-06 16:12:35 -0600

    changed alt text in image, removed extra class

--- index.php
+++ index.php
@@ -13,15 +13,15 @@
      </div>
        <div class="container">
  <div class="img-responsive col-sm-4">
-   <img id="board" src="assets/img/6796009882_074f87df69_b.jpg" class="img-rounded" alt="Skateboard" height="187" width="333">
+   <img id="board" src="assets/img/6796009882_074f87df69_b.jpg" alt="Skateboard" height="187" width="333">
  </div>
  
  <div class="img-responsive col-sm-4">
-   <img id="long" src="assets/img/18752831476_4634222c05_b.jpg" class="img-rounded" alt="Longboard" height="187" width="333">
+   <img id="long" src="assets/img/18752831476_4634222c05_b.jpg" alt="Longboard" height="187" width="333">
  </div>
  
  <div class="img-responsive col-sm-4">
-   <img id="truck" src="assets/img/maxresdefault.jpg" class="img-rounded" alt="Skate Wheel" height="187" width="333">
+   <img id="truck" src="assets/img/maxresdefault.jpg" alt="Skate Truck" height="187" width="333">
  </div>
  
 </div> 
