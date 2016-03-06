7937183bc218bcbcd56060870860d6395aa8fe45
nathangruber <grubernp@gmail.com>
2016-03-06 12:45:46 -0600

    adding logo to navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,7 +7,7 @@
          <span class="icon-bar"></span>
          <span class="icon-bar"></span>
        </button>
-       <a class="navbar-brand" href="index.php" id="black"></a>
+       <a class="navbar-brand" href="index.php" id="black"><img class="img-responsive logo" src="assets/img/bbs.png"></a>
      </div>
      <div id="navbar" class="collapse navbar-collapse">
        <ul class="nav navbar-nav">
