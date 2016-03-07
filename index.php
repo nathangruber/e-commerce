9e5932d4ca2e92928077de295025db6f1b68491d
nathangruber <grubernp@gmail.com>
2016-03-06 18:33:01 -0600

    adjusting twitter link

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <div id="container">
 
 <div class="leftimage"><img src="assets/img/bbs.png"></div>
-<div class="righttext">Blake’s is a locally-owned skateboard shop. <a href="https://twitter.com/varnathangruber" class="twitter-follow-button" data-show-count="false">Follow @varnathangruber</a>
+<div class="righttext">Blake’s is a locally-owned skateboard shop. <a href="https://twitter.com/varnathangruber" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a>
  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
    </div> 
 
