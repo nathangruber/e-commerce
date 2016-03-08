18042edbbb368536cd9d83127d6679c4beb28875
nathangruber <grubernp@gmail.com>
2016-03-07 21:50:31 -0600

    included session.php

--- userupdate.php
+++ userupdate.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL);
+  error_reporting(E_ALL);
   require_once 'includes/database.php';
+  require_once('includes/session.php');
 
   if ( !empty($_POST)) {
 
@@ -11,15 +12,15 @@ error_reporting(E_ALL);
     $phone_number = $_POST['phone_number'];
     $email_address = $_POST['email_address'];
     
-    echo $id . "<br>" . $name . "<br>" . $birth_date . "<br>" . $phone_number . "<br>" . $email_address . "<br>";
+    //echo $id . "<br>" . $name . "<br>" . $birth_date . "<br>" . $phone_number . "<br>" . $email_address . "<br>";
 
     function valid($uservar){
       return ( !empty($uservar) && isset($uservar) );
     }
 
     if ( !valid($name) || !valid($birth_date) || !valid($phone_number) || !valid($email_address) || !filter_var($email_address,FILTER_VALIDATE_EMAIL) ) {
-      echo "died 1";
-      die();
+      //echo "died 1";
+      //die();
       header("Location: update.php");
     }
 
@@ -30,8 +31,8 @@ error_reporting(E_ALL);
       $q = $pdo->prepare($sql);
       $q->execute(array($name,$birth_date,$phone_number,$email_address,$id));
       Database::disconnect();
-      echo "queried";
-      die();
+      //echo "queried";
+      //die();
       header("Location: update.php");
     } catch (PDOException $error){
       echo "Error: " . $error->getMessage();
@@ -40,7 +41,7 @@ error_reporting(E_ALL);
     }
 
   } else {
-    echo "empty post";
-    die();
+    //echo "empty post";
+    //die();
     header("Location: update.php");
   }
