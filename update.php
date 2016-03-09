8a824385c3e8cf04414d9b844af75ab7f1b08c11
nathangruber <grubernp@gmail.com>
2016-03-08 18:51:53 -0600

    edited sql query-update.php

--- update.php
+++ update.php
@@ -69,7 +69,7 @@ Database::disconnect();
           if($logged) {
               $pdo = Database::connect();
               $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              $sql = 'SELECT * FROM customer WHERE id = ?';
+              $sql = 'SELECT * FROM address WHERE id = ?';
               $q = $pdo->prepare($sql);
               $q->execute(array($_SESSION["id"]));
               $query = $q->fetch(PDO::FETCH_ASSOC);
