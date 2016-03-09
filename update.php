8656b98d514cbf852d06398a98aaf2f9bb051e27
nathangruber <grubernp@gmail.com>
2016-03-08 18:48:06 -0600

    added header tag- update.php

--- update.php
+++ update.php
@@ -10,7 +10,7 @@
     require_once('includes/database.php');
     error_reporting(E_ALL);
   ?>
-
+<h3></h3>
   <div class="container">
     <div class="row">
       <table class="table table-striped table-bordered">
