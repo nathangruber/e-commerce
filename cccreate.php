f90e22ebc1ccfb3caa716f4d1aa374fb658c3c8c
nathangruber <grubernp@gmail.com>
2016-03-09 16:46:34 -0600

    removed additional SQL query

--- cccreate.php
+++ cccreate.php
@@ -126,23 +126,6 @@
           </div>
 
           <br>
-
-          <?php
-            try {
-              $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              $sql = "SELECT `address`.`id`, `address`.`street_1` FROM `address` LEFT JOIN `customer_address` ON `address`.`id`=`customer_address`.`address_fk` WHERE (`customer_address`.`customer_fk` = ". $_SESSION['id'] . ")";
-              $address = $pdo->query($sql);
-             // echo "<select name='address_fk'>";
-              // foreach ($address as $row) {
-              // echo "<option value='" . $row['id'] . "'>" . $row['street_1'] . "</option>";
-            //  }
-              echo "</select>";
-            } catch (PDOException $e) {
-              echo $e->getMessage();
-            }
-          ?>
-
-          <br>
           <br>
           <br>
           <div class="form-actions">
