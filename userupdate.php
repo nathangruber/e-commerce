4064763a04044e7667c69608a927ea9a44be0510
nathangruber <grubernp@gmail.com>
2016-03-11 16:53:59 -0600

    added gender var

--- userupdate.php
+++ userupdate.php
@@ -6,7 +6,7 @@
   }
   require_once('includes/database.php');
   require_once('includes/crud.php');
-  $pdo = Database::connect();
+
 
 
   if ( !empty($_POST)) {
@@ -16,48 +16,15 @@
     $birth_date = $_POST['birth_date'];
     $phone_number = $_POST['phone_number'];
     $email_address = $_POST['email_address'];
+    $gender = $_POST['gender'];
     
-     $updatecustomer = new customer();
-      $response = $updatecustomer->update($name,$birth_date,$phone_number,$email_address,$_SESSION['id']);
-      if ($response) {
-        header('Location: update.php');
-      } else {
-        header('Location: update.php?error=1');
-      }
-    }  
-
-    //echo $id . "<br>" . $name . "<br>" . $birth_date . "<br>" . $phone_number . "<br>" . $email_address . "<br>";
-
-   /* function valid($uservar){
-      return ( !empty($uservar) && isset($uservar) );
+    $updatecustomer = new customer();
+    $response = $updatecustomer->update($name,$birth_date,$gender, $phone_number,$email_address,$_SESSION['id']);
+    if ($response) {
+      header('Location: update.php');
+    } else {
+      header('Location: update.php?error=1');
     }
+  }  
 
-    if ( !valid($name) || !valid($birth_date) || !valid($phone_number) || !valid($email_address) || !filter_var($email_address,FILTER_VALIDATE_EMAIL) ) {
-      //echo "died 1";
-      //die();
-      header("Location: update.php");
-    }
-
-    try {
-      
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sql = "UPDATE customer SET name = ?, birth_date = ?, phone_number = ?, email_address = ? WHERE id = ?";
-      $q = $pdo->prepare($sql);
-      $q->execute(array($name,$birth_date,$phone_number,$email_address,$id));
-   
-      //echo "queried";
-      //die();
-      header("Location: update.php");
-    } catch (PDOException $error){
-      echo "Error: " . $error->getMessage();
- 
-      die();
-    }
-
-  } else {
-    //echo "empty post";
-    //die();
-    header("Location: update.php");
-  }
-  ?>
-  <?php Database::disconnect(); ?>
+    
