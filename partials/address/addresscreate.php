f5c11e66893a06abb29df5ba8fc9ab874d7b077f
nathangruber <grubernp@gmail.com>
2016-03-12 12:05:03 -0600

    updated address create in partials/admin

--- partials/address/addresscreate.php
+++ partials/address/addresscreate.php
@@ -4,28 +4,23 @@
     require_once 'includes/navbar.php';
     if ( !empty($_POST)) {
         // keep track validation errors
-        $nameError = null;
+       
         $street_1Error = null;
         $street_2Error = null;
         $cityError = null;
         $stateError = null;
         $zip_codeError = null;
-        $countryError = null;
+        
         // keep track post values
-        $name = $_POST['name'];
+        
         $street_1 = $_POST['street_1'];
         $street_2 = $_POST['street_2'];
         $city = $_POST['city'];
         $state = $_POST['state'];
         $zip_code = $_POST['zip_code'];
-        $country = $_POST['country'];
+        
         // validate input
        $valid = true;
-        if (empty($name)) {
-            $nameError = 'Please enter Name';
-            $valid = false;
-        }
-         
         if (empty($street_1)) {
             $street_1Error = 'Please enter Valid Street Address';
             $valid = false;
@@ -47,30 +42,27 @@
             $zip_codeError = 'Please enter Zip Code';
             $valid = false;
         }
-        if (empty($country)) {
-            $countryError = 'Please enter Country';
-            $valid = false;
-        }
-       
+        
         // insert data
         if ($valid) {
-	
-            $pdo = Database::connect();
+	        $pdo = Database::connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO address (name,street_1,street_2,city,state,zip_code,country) values(?, ?, ?, ?, ?, ?, ?)";
+            $sql = "INSERT INTO address (street_1,street_2,city,state,zip_code) values(?, ?, ?, ?, ?)";
             $q = $pdo->prepare($sql);
-            $q->execute(array($name,$street_1,$street_2,$city,$state,$zip_code,$country));
+            $q->execute(array($street_1,$street_2,$city,$state,$zip_code));
             Database::disconnect();
             header("Location: index.php");
 	}
-    }
+ }
 ?>
 <!DOCTYPE html>
 
 <html lang="en">
 <head>
     <meta charset="utf-8">
-   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       
+   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
  
 <body>
@@ -82,15 +74,6 @@
                     </div>
              
                     <form class="form-horizontal" action="create.php" method="post">
-                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
-                        <label class="control-label">Name</label>
-                        <div class="controls">
-                            <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
-                            <?php if (!empty($nameError)): ?>
-                                <span class="help-inline"><?php echo $nameError;?></span>
-                            <?php endif; ?>
-                        </div>
-                      </div>
                       <div class="control-group <?php echo !empty($street_1Error)?'error':'';?>">
                         <label class="control-label">Street Address</label>
                         <div class="controls">
@@ -139,20 +122,7 @@
                             <?php endif;?>
                         </div>
                       </div>
-                      <div class="control-group <?php echo !empty($countryError)?'error':'';?>">
-                        <label class="control-label">Country</label>
-                        <div class="controls">
-                            <input name="country" type="text"  placeholder="Country" value="<?php echo !empty($country)?$country:'';?>">
-                            <?php if (!empty($countryError)): ?>
-                                <span class="help-inline"><?php echo $countryError;?></span>
-                            <?php endif;?>
-                        </div>
-                      </div>
-
-
-
-
-
+                      
                       <div class="form-actions">
                           <button type="submit" class="btn btn-success">Create</button>
                           <a class="btn" href="index.php">Back</a>
