b5837861a374e54e80f95a884fc986600500747c
nathangruber <grubernp@gmail.com>
2016-03-15 17:57:10 -0500

    created about.php

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php require_once'includes/session.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php 
+    if ($admin) {
+      require_once'includes/adminNavbar.php';
+    } else {
+      require_once'includes/navbar.php';
+    }
+    ?>
+ <?php require_once 'includes/header.php';?>
+     <title>About Blake's Board Shop</title>
+ </head>
+<body>
+
+      <div class="container">
+        <div class = "row">
+        <div class="col-md-12">
+<div class = "abouttext">
+
+
+<p>Blake's Board Shop is family owned and operated.</p></div>
+        </div>
+      </div>
+    </div>
+        
+<div class="image">
+<img src="assets/img/bbs1.png" class="img-rounded" alt="Thumbs up" width="640" height="1150">
+</div>
+
+
+
+
+
+   
+<?php
+require_once('footer.php');
+?>
+   
+
+   
+  </body>
+</html>
