e14f026294390625a05478f4e046bc4ec20e3994
nathangruber <grubernp@gmail.com>
2016-03-15 23:13:00 -0500

    testing dropdowns

--- productPage.php
+++ productPage.php
@@ -10,7 +10,18 @@
     Dropdown
     <span class="caret"></span>
   </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+  <ul class="dropdown-menu" aria-labelledby="Skateboards">
+    <li><a href="#"></a>Decks</li>
+    <li><a href="#">Completes</a></li>
+    
+  </ul>
+</div>
+<div class="dropdown col-sm-4">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    Dropdown
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu " aria-labelledby="Longboards">
     <li><a href="#">Action</a></li>
     <li><a href="#">Another action</a></li>
     <li><a href="#">Something else here</a></li>
@@ -23,20 +34,7 @@
     Dropdown
     <span class="caret"></span>
   </button>
-  <ul class="dropdown-menu " aria-labelledby="dropdownMenu1">
-    <li><a href="#">Action</a></li>
-    <li><a href="#">Another action</a></li>
-    <li><a href="#">Something else here</a></li>
-    <li role="separator" class="divider"></li>
-    <li><a href="#">Separated link</a></li>
-  </ul>
-</div>
-<div class="dropdown col-sm-4">
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    Dropdown
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+  <ul class="dropdown-menu" aria-labelledby="Hardware">
     <li><a href="#">Action</a></li>
     <li><a href="#">Another action</a></li>
     <li><a href="#">Something else here</a></li>
