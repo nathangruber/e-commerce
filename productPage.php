1a43aedf73b844ab8b81e6630ee0422481559c3a
nathangruber <grubernp@gmail.com>
2016-03-15 23:09:28 -0500

    testing

--- productPage.php
+++ productPage.php
@@ -5,7 +5,7 @@
 		<body>
 
 			<?php require_once 'includes/navbar.php';?>
-<div class="dropdown">
+<div class="dropdown col-sm-4">
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
     Dropdown
     <span class="caret"></span>
@@ -18,12 +18,12 @@
     <li><a href="#">Separated link</a></li>
   </ul>
 </div>
-<div class="dropdown">
+<div class="dropdown col-sm-4">
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
     Dropdown
     <span class="caret"></span>
   </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+  <ul class="dropdown-menu " aria-labelledby="dropdownMenu1">
     <li><a href="#">Action</a></li>
     <li><a href="#">Another action</a></li>
     <li><a href="#">Something else here</a></li>
@@ -31,7 +31,7 @@
     <li><a href="#">Separated link</a></li>
   </ul>
 </div>
-<div class="dropdown">
+<div class="dropdown col-sm-4">
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
     Dropdown
     <span class="caret"></span>
