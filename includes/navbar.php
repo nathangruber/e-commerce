035be99396ad24861d6e499896cfb9fbc1fa80cb
nathangruber <grubernp@gmail.com>
2016-03-15 21:20:22 -0500

    testing navbar with dropdown

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,6 +16,15 @@
          <li><a href="index.php" class="img-responsive logo" src="assets/img/bbs.png"></a></li>
          <li><a href="index.php" class="black">Blake's Board Shop</a></li>
          <li><a href="register.php" class="black">Register</a></li>
+         <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Products
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="#">Skateboards</a></li>
+      <li><a href="#">Longboards</a></li>
+      <li><a href="#">Hardware</a></li>
+    </ul>
+  </div>
          <li><a href="productPage.php" class="black">Products</a></li> 
          <li><a href="about.php" class="black">About</a></li>
          <li><a href="contact.php" class="black">Contact Us</a></li>
