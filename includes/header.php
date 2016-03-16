8188a4a35c0b19d97fd3a48b9ba58ee6104c35b2
nathangruber <grubernp@gmail.com>
2016-03-15 19:42:02 -0500

    adjusting favicon

--- includes/header.php
+++ includes/header.php
@@ -6,4 +6,5 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
         <link href='https://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css'>
+		<link rel='shortcut icon' href='favicon (2).ico' type='image/x-icon'/ >
 </head>
