e553ea673a4d9ea00cdb1096efe456a7111c02c3
nathangruber <grubernp@gmail.com>
2016-03-16 21:07:46 -0500

    adding id functionality to nav
    testing it on index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     if ($admin) {
       require_once'includes/adminNavbar.php';
     } else {
-      require_once'includes/navbar.php';
+      require_once'includes/navbarID.php';
     }
     ?>
       <?php
