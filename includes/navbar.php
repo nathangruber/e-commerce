48e83234b2f58a9e5c952eda34f60a9506632a19
nathangruber <grubernp@gmail.com>
2016-03-17 13:49:42 -0500

    updating navbar across all pages

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,27 +1,67 @@
-<nav class="navbar navbar-inverse">
-   <div class="container">
-     <div class="navbar-header">
-       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-         <span class="sr-only">Toggle navigation</span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-       </button>
-       <div>
-       <a class="navbar-brand" href="index.php" id="black"><img class="img-responsive logo" src="assets/img/bbs.png" height="27" width="27"></a>
-      </div>
+<nav class="navbar navbar-default navbar-fixed-top">
+        
+  <div class="container">
+    <div class="navbar-header">
+          
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+         <a class="navbar-brand" href="index.php" id="black"><img src="assets/img/bbs.png" height="27" width="27"></a>   
      </div>
-     <div id="navbar" class="collapse navbar-collapse">
-       <ul class="nav navbar-nav">
-         <li><a href="index.php" class="img-responsive logo" src="assets/img/bbs.png"></a></li>
-         <li><a href="index.php" class="black">Blake's Board Shop</a></li>
-         <li><a href="register.php" class="black">Register</a></li>
-         <li><a href="category.php" class="black">Products</a></li>
-         <li><a href="about.php" class="black">About</a></li>
-         <li><a href="contact.php" class="black">Contact Us</a></li>
-       </ul>
-     </div>
-   </div>
- </nav>
 
- 
+         
+   <div id="navbar" class="collapse navbar-collapse">
+            
+    <ul class="nav navbar-nav">
+            
+    <li><a href="index.php">Blake's Board Shop</a></li>
+
+                      
+              <?php 
+    require_once('database.php');
+    $pdo = Database::connect();
+          $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $sql = "SELECT * FROM category ORDER BY name ASC";
+    
+    echo '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="category.php">Products<span class="caret"></span></a>';
+          echo '<ul class="dropdown-menu">';
+            
+            foreach ($pdo->query($sql) as $category) {
+              echo '<li id="' . $category['name'] . '">';
+                echo '<a href="category.php?id=' . $category['id'] . '">';
+                echo ' ' . $category['name'] . ' ';
+                echo '</a>';
+              echo '</li>';
+            
+            }
+            echo '</ul>';
+           echo '</li>';  
+    Database::disconnect();
+  
+     ?>                         
+                    
+  
+
+    <li><a href="register.php">Register</a></li>
+    <li><a href="loginpage.php">Login</a></li>
+    <li><a href="about.php" class="black">About</a></li>
+    <li><a href="contact.php" class="black">Contact Us</a></li>
+    
+    <?php if( isset($_SESSION['permission']) && $_SESSION['permission'] == 0){?> 
+      <li><a href="update.php">Update</a></li>
+    <?php } ?>
+
+    
+    <?php if ( isset($_SESSION['permission']) && $_SESSION['permission'] == 1) {?>
+      <li><a href="update.php">Update</a></li>  
+      <li><a href="admin.php">Admin</a></li>
+    <?php } ?>
+
+     
+      </ul>
+          </div>
+        </div>
+      </nav>
