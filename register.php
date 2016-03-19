68d6c06a10621062ff0469bacac8be21ceba84fb
nathangruber <grubernp@gmail.com>
2016-03-19 00:24:19 -0500

    styled bootstrap buttons

--- register.php
+++ register.php
@@ -172,7 +172,7 @@
 
                              <div class="form-actions">
                           <button type="submit" class="btn btn-success">Create</button>
-                          <a class="btn" href="index.php">Back</a>
+                          <a class="btn btn-default" href="index.php">Back</a>
                         </div>
                     </form>
                     <?php }else{ ?>
