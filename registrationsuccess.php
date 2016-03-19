ffca28176c9a43e74286e167d072feaa0cc90b19
nathangruber <grubernp@gmail.com>
2016-03-19 12:00:58 -0500

    testing reg success page

--- registrationsuccess.php
+++ registrationsuccess.php
@@ -4,13 +4,19 @@
 <?php require_once 'includes/header.php';?>
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 <body>
-
+<?php 
+    if ($admin) {
+      require_once'includes/adminNavbar.php';
+    } else {
+      require_once'includes/navbar.php';
+    }
+    ?>
 <h3>Successfully Registered</h3>
 <p>You have created an account at BBS, please login.
 <p>go <a class="btn btn-default" href="index.php">back</a> or <a class="btn btn-default" href="loginpage.php">login</a>.</p>
 
 
-<?php require_once 'includes/navbar.php'; ?>
+
 <?php require_once 'includes/footer.php'; ?>
 
 	</body>
