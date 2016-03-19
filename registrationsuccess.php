22b45ceb4f48db9431989721879e7e13dc113f99
nathangruber <grubernp@gmail.com>
2016-03-19 11:56:49 -0500

    editing reg success

--- registrationsuccess.php
+++ registrationsuccess.php
@@ -5,9 +5,8 @@
 <html>
 <?php require_once 'includes/header.php';?>
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-
-<body>
 <h3>Successfully Registered</h3>
+<body>
 <br><br><br>
 
 <center><p>go <a class="btn btn-default" href="index.php">back</a> or <a class="btn btn-default" href="loginpage.php">login</a>.</p></center>
