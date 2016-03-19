fba9a91b010c4037f83ed9deff236f9d6184127c
nathangruber <grubernp@gmail.com>
2016-03-19 11:58:18 -0500

    reg success edit

--- registrationsuccess.php
+++ registrationsuccess.php
@@ -1,18 +1,16 @@
 <?php require_once 'includes/session.php'; ?>
-<?php require_once 'includes/navbar.php'; ?>
 
 <!DOCTYPE html>
 <html>
 <?php require_once 'includes/header.php';?>
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-<h3>Successfully Registered</h3>
 <body>
-<br><br><br>
 
-<center><p>go <a class="btn btn-default" href="index.php">back</a> or <a class="btn btn-default" href="loginpage.php">login</a>.</p></center>
+<h3>Successfully Registered</h3>
+<p>go <a class="btn btn-default" href="index.php">back</a> or <a class="btn btn-default" href="loginpage.php">login</a>.</p>
 
 
-
+<?php require_once 'includes/navbar.php'; ?>
 <?php require_once 'includes/footer.php'; ?>
 
 	</body>
