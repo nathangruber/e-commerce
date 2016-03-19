eb80d6400eef69f43f3e33d3c2b3e7cb384fd2d1
nathangruber <grubernp@gmail.com>
2016-03-18 23:32:10 -0500

    removed header.php

--- cccreate.php
+++ cccreate.php
@@ -62,7 +62,15 @@
 
 <!DOCTYPE html>
 <html lang="en">
- <?php require_once 'header.php';?>
+ <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">  
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <title>Add a Credit Card</title>
+ </head>
   
   <body>
    
