a17d0621ca31adbb4202e3308fd536b58bb45fb2
nathangruber <grubernp@gmail.com>
2016-03-19 11:54:22 -0500

    fixing navbar

--- registrationsuccess.php
+++ registrationsuccess.php
@@ -5,11 +5,9 @@
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 
 <body>
-<?php require_once 'includes/navbar.php';?>
-
 <h3>Successfully Registered</h3>
 <br><br><br>
-<p>Successfully Registered</p>
+<center><p>Successfully Registered</p></center>
 <p>go <a class="btn btn-default" href="index.php">back</a> or <a class="btn btn-default" href="loginpage.php">login</a>.</p>
 
 
