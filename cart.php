258bd246fd702dfd78acbb0988057ff2fb9bf615
nathangruber <grubernp@gmail.com>
2016-03-19 08:23:15 -0500

    dolar

--- cart.php
+++ cart.php
@@ -54,7 +54,7 @@ require_once 'includes/crud.php';
 				
 				<tr>
 					<td><?php echo $product_details['product_name']; ?></td>
-					<td><?php echo money_format('%i', $product_details['price']); ?></td>
+					<td><?php echo money_format('$', $product_details['price']); ?></td>
 					<td><?php echo $row['quantity']; ?><a class="btn btn-default" style="margin-left: 30px" href="updateQuantity.php?type=more">+</a><a class="btn btn-default" href="updateQuantity.php?type=less">-</a></td>
 					<td>$<?php echo $subtotal_product; ?></td>
 					<td><a class="btn btn-danger" href="removeItem.php">Remove item</a></td>
