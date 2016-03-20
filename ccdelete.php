888922beec6168a747e65a64971884cb72429876
nathangruber <grubernp@gmail.com>
2016-03-20 18:58:30 -0500

    tabbing edit

--- ccdelete.php
+++ ccdelete.php
@@ -1,27 +1,26 @@
  <?php
-  require_once('includes/session.php');
-  if(!$logged){
-    header("Location: index.php");
-    die(); // just in case
-  }
-  require_once('includes/database.php');
-  require_once('includes/crud.php');
+require_once('includes/session.php');
+if(!$logged){
+	header("Location: index.php");
+	die(); // just in case
+}
+require_once('includes/database.php');
+require_once('includes/crud.php');
 
 
 
 
- 
-  if ( !empty($_POST['creditcard_id']) && isset($_POST['creditcard_id'])) {
 
-    $myCreditcards = new customerCreditcards($_SESSION['id']);
-    $response = $myCreditcards->delete($_POST['creditcard_id']);
-    
+if ( !empty($_POST['creditcard_id']) && isset($_POST['creditcard_id'])) {
 
-    if ($response) {
-      header('Location: update.php?feedbackdeleted=ok');
-    } else {
-      header('Location: update.php?feedbackdeleted=error');
-    }
+	$myCreditcards = new customerCreditcards($_SESSION['id']);
+	$response = $myCreditcards->delete($_POST['creditcard_id']);
 
 
-  }
+	if ($response) {
+		header('Location: update.php?feedbackdeleted=ok');
+	} else {
+		header('Location: update.php?feedbackdeleted=error');
+	}
+
+}
