0774b0440c862a0d2b350385d8b653016b4715fe
nathangruber <grubernp@gmail.com>
2016-03-20 17:09:48 -0500

    checkout final

---
+++ checkout-final.php
@@ -0,0 +1,49 @@
+<?php
+require_once'includes/session.php';
+require_once'includes/database.php';
+require_once'includes/crud.php';
+error_reporting(E_ALL);
+Database::connect();
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<?php require_once 'includes/header.php';?>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <?php
+    if ($admin) {
+        require_once'includes/adminNavBar.php';
+    } else {
+        require_once'includes/navbar.php';
+    }
+    ?>
+
+    <div class="container">
+        <div class="row">
+        	<div class="span12">
+	        	<h1 style="margin-top: 100px">Thank you !!</h1>
+	        	
+	        	<?php
+		        	$address_fk = $_POST['address_fk'];
+		        	$credit_card_fk = $_POST['credit_card_fk'];
+		        	
+		        	print_r($_POST);
+		        		
+		        ?>
+	        	Your transaction was... 
+	        	
+        	</div>          
+        </div>
+      
+    </div><!-- /container -->
+    <?php
+    require_once('includes/footer.php');
+    Database::disconnect();
+    ?>
+</body>
+</html>
