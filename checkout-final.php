03ea1c39602eac4744dbc16c82cb241dbcabad1b
nathangruber <grubernp@gmail.com>
2016-03-20 18:31:33 -0500

    tabbed code

--- checkout-final.php
+++ checkout-final.php
@@ -16,47 +16,47 @@ Database::connect();
 
 <body>
     <?php
-    if ($admin) {
-        require_once'includes/adminNavBar.php';
-    } else {
-        require_once'includes/navbar.php';
-    }
-    ?>
+if ($admin) {
+	require_once'includes/adminNavBar.php';
+} else {
+	require_once'includes/navbar.php';
+}
+?>
 
     <div class="container">
         <div class="row">
         	<div class="span12">
 	        	<h1 style="margin-top: 100px">Thank you !!</h1>
-	        	
+
 	        	<?php
-		        	$address_fk = $_POST['address_fk'];
-		        	$credit_card_fk = $_POST['credit_card_fk'];
-		        	
-		        	$address = new customerAddress($_SESSION['id']);
-		        	$the_address=$address->readAnAddress($address_fk);
-		        	
-		        	
-		        	echo 'Your transaction will be sent to'.$the_address['street_1'].' , '.$the_address['street_2'];
-		        	
-		        	echo '<br>';
-		        	$cc = new customerCreditcards($_SESSION['id']);
-		        	$the_cc=$cc->readACreditCard($credit_card_fk);
-		        	
-		        	echo 'And will be charged to'.$the_cc['name'].' , '.$the_cc['cardnumber'];
-		        	
-		        	echo '<br>';
-		        	
-		        	
-		        		
-		        ?>
-	        	
-        	</div>          
+$address_fk = $_POST['address_fk'];
+$credit_card_fk = $_POST['credit_card_fk'];
+
+$address = new customerAddress($_SESSION['id']);
+$the_address=$address->readAnAddress($address_fk);
+
+
+echo 'Your transaction will be sent to'.$the_address['street_1'].' , '.$the_address['street_2'];
+
+echo '<br>';
+$cc = new customerCreditcards($_SESSION['id']);
+$the_cc=$cc->readACreditCard($credit_card_fk);
+
+echo 'And will be charged to'.$the_cc['name'].' , '.$the_cc['cardnumber'];
+
+echo '<br>';
+
+
+
+?>
+
+        	</div>
         </div>
-      
+
     </div><!-- /container -->
     <?php
-    require_once('includes/footer.php');
-    Database::disconnect();
-    ?>
+require_once('includes/footer.php');
+Database::disconnect();
+?>
 </body>
 </html>
