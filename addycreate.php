7a60c9c2a07750558183db5cf95ba12398cc720a
nathangruber <grubernp@gmail.com>
2016-03-20 14:35:38 -0500

    added header.php

--- addycreate.php
+++ addycreate.php
@@ -71,16 +71,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
- <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-
-    <title>Register</title>
- </head>
-  
+<?php require_once 'includes/header.php';?>
+  <title>Register</title>
   <body>
     <?php require_once('includes/navbar.php');?>
 
