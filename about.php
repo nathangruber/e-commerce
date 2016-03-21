0f49817e1905a9c1213b26c19c4e3ac8a3da16b5
nathangruber <grubernp@gmail.com>
2016-03-20 21:02:28 -0500

    img edit

--- about.php
+++ about.php
@@ -20,7 +20,7 @@ if ($admin) {
     <div class="container">
         <div class="row">
             <div class="container" style="width: 100%;">
-                <div class="theme-table-image col-sm-6"><img src="assets/img/0A32B0986D.jpg" alt="Skater"></div>
+                <div class="theme-table-image col-sm-6"><img src="assets/img/0A32B0986D.jpg"></div>
 
                 <div class="theme-table-image col-sm-6"><img src="assets/img/99factory_slider_05.jpg"></div>
             </div>
