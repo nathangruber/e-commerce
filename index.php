9352da2caeb373d9e1919e0dcba1519cc0752a7f
nathangruber <grubernp@gmail.com>
2016-03-20 23:05:29 -0500

    added additional img links

--- index.php
+++ index.php
@@ -38,11 +38,13 @@
   </div>
  
    <div class="img-responsive col-sm-4">
+    <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/e-commerce/category.php?id=10" id="black">
     <img id="long" src="assets/img/18752831476_4634222c05_b.jpg" alt="Longboard" height="187" width="333">
      </a>
   </div>
  
    <div class="img-responsive col-sm-4">
+    <a href= "http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/e-commerce/category.php?id=12" id="black">
     <img id="truck" src="assets/img/maxresdefault.jpg" alt="Skate Truck" height="187" width="333">
      </a>
    </div>
