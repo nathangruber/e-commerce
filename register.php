9e47c010b52a5cf74051a43fd9e74c06fe1c04b8
nathangruber <grubernp@gmail.com>
2016-03-20 20:51:20 -0500

    removed script

--- register.php
+++ register.php
@@ -181,7 +181,6 @@
                 </div>
                  
     </div> <!-- /container -->
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <?php
 require_once 'includes/footer.php';
 ?>
