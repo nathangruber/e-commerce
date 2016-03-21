2f7afe93774230b86d6eb3edd7cd87307d368e84
nathangruber <grubernp@gmail.com>
2016-03-20 21:04:12 -0500

    adjusting image

--- about.php
+++ about.php
@@ -20,12 +20,12 @@ if ($admin) {
     <div class="container">
         <div class="row">
             <div class="container" style="width: 100%;">
-                <div class="theme-table-image col-sm-6"><img src="assets/img/0A32B0986D.jpg"></div>
+                <div class="theme-table-image col-sm-4"><img src="assets/img/0A32B0986D.jpg"></div>
 
-                <div class="theme-table-image col-sm-6"><img src="assets/img/99factory_slider_05.jpg"></div>
+                <div class="theme-table-image col-sm-4"><img src="assets/img/99factory_slider_05.jpg"></div>
             </div>
 
-            <p>Blake's Board Shop makes high quality, made in Wisconsin skateboard and cruiser decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop.</p>
+            <div class="theme-table=image col-sm-4"><p>Blake's Board Shop makes high quality, made in Wisconsin skateboard and cruiser decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop.</p>
         </div>
     </div><?php
 require_once('includes/footer.php');
