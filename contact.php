e0697c76cab65889891b4b44c4ab096772041455
nathangruber <grubernp@gmail.com>
2016-03-26 22:46:05 -0500

    added <br>

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 <?php require_once 'includes/header.php';?>
 
 <head>
-    <title></title>
+    <title>Contact Us</title>
 </head>
 
 <body>
@@ -22,7 +22,7 @@ if ($admin) {
             <div class="col-md-4">
                 <div class="contactus">
                     <p>Contact Us</p>
-
+<br>
                     <form name="contactform" method="post" action="emailform.php">
                         <label for="first_name">First Name *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> <input type="text" name="first_name" maxlength="45" size="30"> <label for="last_name">Last Name *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> <input type="text" name="last_name" maxlength="45" size="30"> <label for="email">Email Address *</label> <input type="text" name="email" maxlength="45" size="30"> <label for="telephone">Phone Number*</label> <input type="text" name="telephone" maxlength="30" size="30"> <label for="comments">Comments *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
                         <textarea name="comments" maxlength="1000" cols="30" rows="5">
