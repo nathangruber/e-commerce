bd4370d47954a223dec7c5c2ac28b16dd6982e9f
nathangruber <grubernp@gmail.com>
2016-03-26 23:21:34 -0500

    added title and centered buttons

--- registrationsuccess.php
+++ registrationsuccess.php
@@ -1,22 +1,28 @@
 <?php require_once 'includes/session.php'; ?>
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
 <html>
 <?php require_once 'includes/header.php';?>
+
+<head>
+    <title>Success</title>
+</head>
+
 <body>
-<?php 
+    <?php
     if ($admin) {
-      require_once'includes/adminNavbar.php';
+        require_once'includes/adminNavbar.php';
     } else {
-      require_once'includes/navbar.php';
+        require_once'includes/navbar.php';
     }
     ?>
-<h3>Successfully Registered</h3>
-<center><p>You have successfully created an account at BBS, please login.</center>
-<p>go <a class="btn btn-default" href="index.php">Back</a> or <a class="btn btn-default" href="loginpage.php">Login</a>.</p>
 
+    <h3>Successfully Registered</h3>
 
+    <center>
+        <p>You have successfully created an account at BBS, please login.</p>
+    </center>
 
-<?php require_once 'includes/footer.php'; ?>
-
-	</body>
+    <p>go <a class="btn btn-default" href="index.php">Back</a> or <a class="btn btn-default" href="loginpage.php">Login</a>.</p><?php require_once 'includes/footer.php'; ?>
+</body>
 </html>
