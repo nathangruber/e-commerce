3953d7b5b2629711b7cef237e7417537a530dc3d
nathangruber <grubernp@gmail.com>
2016-03-26 22:22:21 -0500

    Revert "cleaned html"
    This reverts commit d16cc2151fc6025c460f82446ab2245bb367980d.

--- category.php
+++ category.php
@@ -1,65 +1,57 @@
 <?php require_once 'includes/session.php'; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-
+<!DOCTYPE html>
 <html>
 <?php require_once 'includes/header.php'; ?>
 
-<head>
-    <title></title>
-</head>
-
 <body>
-    <?php require_once 'includes/navbar.php'; ?><br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+<?php require_once 'includes/navbar.php'; ?>
+<br><br><br><br><br><br>
 
-    <div class="container">
-        <div class="row">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Name</th>
 
-                        <th>Price</th>
 
-                        <th>Description</th>
-                    </tr>
-                </thead>
+<div class="container">
+      <div class="row">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+        <tbody>
+    
+              <?php 
+              $category_id = $_GET['id'];
 
-                <tbody>
-                    <?php 
-                                  $category_id = $_GET['id'];
+              $product = new product();
+              $products = $product->readAllCategory($category_id);
 
-                                  $product = new product();
-                                  $products = $product->readAllCategory($category_id);
-
-                                  foreach ($products as $row) {
-                                      
-                                  ?>
-
-                    <tr>
-                        <td><?php echo $row['product_name']; ?></td>
-
-                        <td><?php echo $row['price']; ?></td>
-
-                        <td><a class="btn btn-default" href="productPage.php?id=&lt;?php echo $row['id']; ?&gt;">View Product Details</a></td>
-
-                        <td><a class="btn btn-default" href="addCart.php?id=&lt;?php echo $row['id']; ?&gt;">Add to cart</a></td>
-                    </tr><?php
-                                  }  
-                                  ?>
-                </tbody>
-            </table>
-        </div><br>
-        <br>
-        <br>
-        <br>
+              foreach ($products as $row) {
+	              
+	          ?>
+	          	<tr>
+		          	<td><?php echo $row['product_name']; ?></td>
+		          	<td><?php echo $row['price']; ?></td>
+		          	<td><a class="btn btn-default" href="productPage.php?id=<?php echo $row['id']; ?>">View Product Details</a></td>
+		          	<td><a class="btn btn-default" href="addCart.php?id=<?php echo $row['id']; ?>">Add to cart</a></td>
+	          	</tr>
+	          <?php
+		      }  
+		      ?>
+	          
+           </tbody>
+        </table>
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
     </div><!-- /.container -->
-    <br>
-    <br>
-    <?php require_once('includes/footer.php'); ?>
+
+<br><br>
+
+
+<?php require_once('includes/footer.php'); ?>
 </body>
 </html>
