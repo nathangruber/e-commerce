d1c7c0e1ae69d88f048c0ba8e52ad44915d7e06d
nathangruber <grubernp@gmail.com>
2016-03-26 23:02:50 -0500

    cleaning code

--- loginpage.php
+++ loginpage.php
@@ -1,35 +1,38 @@
 <?php require_once 'includes/session.php'; ?>
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
 <html lang="en">
 <?php require_once 'includes/header.php';?>
-  <body>
+
+<head>
+    <title>Login Page</title>
+</head>
+
+<body>
     <?php require_once 'includes/navbar.php';?>
 
-    
     <div class="container">
-      <div class="starter-template">
-        <h1></h1>
-        <p class="lead">Enter Username and Password to login.<br></p>
-      </div>
-      <div>
-        <form action="includes/login.php" method="post">
-          <input type="text" name="username" placeholder="username">
-          <input type="PASSWORD" name="password" placeholder="password">
-          <input class="btn btn-default" type="submit" value="Login">        
-        </form>
-      </div>
-      <div>
-      	<p>No Account? Register below</p>
-      </div>
-      <div>
-        <form action="register.php" method="post">
-          <input class="btn btn-default" type="submit" value="Register">
-        </form>
-      </div>
-    </div>
+        <div class="starter-template">
+            <p class="lead">Enter Username and Password to login.<br></p>
+        </div>
 
-<?php
+        <div>
+            <form action="includes/login.php" method="post">
+                <input type="text" name="username" placeholder="username"> <input type="password" name="password" placeholder="password"> <input class="btn btn-default" type="submit" value="Login">
+            </form>
+        </div>
+
+        <div>
+            <p>No Account? Register below</p>
+        </div>
+
+        <div>
+            <form action="register.php" method="post">
+                <input class="btn btn-default" type="submit" value="Register">
+            </form>
+        </div>
+    </div><?php
 require_once 'includes/footer.php';
 ?>
-  </body>
+</body>
 </html>
