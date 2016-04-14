baa243add17fefd64d80c67ac3b1e38c9f383ae9
nathangruber <grubernp@gmail.com>
2016-04-13 19:25:36 -0500

    continued navbar testing

--- includes/navbar.php
+++ includes/navbar.php
@@ -40,6 +40,29 @@ echo '</li>';
 Database::disconnect();
 
 ?>
+<script>
+function showResult(str) {
+  if (str.length==0) { 
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.send();
+}
+</script>
     <li><a href="register.php">Register</a></li>
     <li><a href="loginpage.php">Login</a></li>
     <li><a href="about.php" >About</a></li>
@@ -60,7 +83,6 @@ Database::disconnect();
 <input type="text" size="30" onkeyup="showResult(this.value)">
 <div id="livesearch"></div>
 </form>
-
     <li><a href="update.php">My Account</a></li>
     <li><a href="cart.php">Cart</a></li>
             </ul>
