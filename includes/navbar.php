165d76e9a2804152f20dd0c2a6a40ffc898af8d9
nathangruber <grubernp@gmail.com>
2016-04-13 20:17:21 -0500

    continued navbar testing

--- includes/navbar.php
+++ includes/navbar.php
@@ -71,18 +71,7 @@ function showResult(str) {
     <li><a href="loginpage.php">Login</a></li>
     <li><a href="about.php" >About</a></li>
     <li><a href="contact.php">Contact Us</a></li>
-    <li><a></a></li>
-    <li><a></a></li>
-	  <li><a></a></li>
-	  <li><a></a></li>
-	  <li><a></a></li>
-	  <li><a></a></li>
-	  <li><a></a></li>
-    <li><a></a></li>
-    <li><a></a></li>
-    <li><a></a></li>
-    <li><a></a></li>
-    <li><a></a></li>
+    
     <li><a href="update.php">My Account</a></li>
     <li><a href="cart.php">Cart</a></li>
             </ul>
