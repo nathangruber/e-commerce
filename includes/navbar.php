0d6989eb71d334fea1882bb3019c1c173408d1fc
nathangruber <grubernp@gmail.com>
2016-04-15 13:14:06 -0500

    removed search

--- includes/navbar.php
+++ includes/navbar.php
@@ -63,45 +63,5 @@ Database::disconnect();
             </ul>
           </div>
         </div>
-      <script type="text/javascript">
-
-   $(document).ready(function(){
-      $( "#find" ).keyup(function(){
-         fetch();
-      });
-   });
-
-   function fetch()
-   {
-   
-      var val = document.getElementById( "find" ).value;
-      $.ajax({
-         type: 'post',
-         url: 'fetch.php',
-         data: {
-            get_val:val
-         },
-         success: function (response) {
-            document.getElementById( "search_items" ).innerHTML = response; 
-         }
-      });
-
-   }
-</script>
-
-    </head>
-
-   <body>
-    <div id="search_box">
-      <center>
-         <p id="heading">Instant FullText Search System Using Ajax And PHP</p>
-         <form method='get' action='fetch.php'>
-         <input type="text" name="get_val" id="find" placeholder="Enter Your Text Here">
-         <input type="submit" name="search" id="search" value="Search">
-         </form>
-
-         <div id="search_items">
-         </div>
-      </center>
-    </div>
+      
       </nav>
