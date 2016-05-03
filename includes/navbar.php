7cb6f8880a7836808caa0ec4e9b1bbb9b3e15579
nathangruber <grubernp@gmail.com>
2016-05-03 17:09:45 -0500

    adjusting search in nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,7 +9,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-         <a class="navbar-brand" href="index.php" id="black"><img src="assets/img/bbs.png" height="26" width="26"></a>
+         <a class="navbar-brand pull-left" href="index.php" id="black"><img src="assets/img/bbs.png" height="26" width="26"></a>
      </div>
 
 
