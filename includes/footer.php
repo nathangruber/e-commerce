2233006c831ccce31b6cb1c14d2f840b182a5eaa
nathangruber <grubernp@gmail.com>
2016-05-21 06:38:45 -0500

    adjusting footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<html>
+<!--<html>
 <head>
     <title>Footer</title>
 </head>
@@ -14,6 +14,65 @@
 </script><script src="assets/js/bootstrap.min.js" type="text/javascript">
 </script>
 <script src="searchFunction.js"></script>
-<!-- <script src="assets/js/hidden.js"></script> -->
+<script src="assets/js/hidden.js"></script> 
 </body>
-</html>
+</html>  -->
+
+
+
+<html>
+<head>
+    <title>Footer</title>
+</head>
+
+<body>
+<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Company<span>logo</span></h3>
+
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Blog</a>
+					·
+					<a href="#">Pricing</a>
+					·
+					<a href="#">About</a>
+					·
+					<a href="#">Faq</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+
+				<p class="footer-company-name">Company Name &copy; 2015</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p>Contact Us</p>
+
+				<form action="#" method="post">
+
+					<input type="text" name="email" placeholder="Email" />
+					<textarea name="message" placeholder="Message"></textarea>
+					<button>Send</button>
+
+				</form>
+
+			</div>
+
+		</footer>
+</body>
+</html> 
