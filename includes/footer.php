9aaa447ed8d85f778f8abd4cdae5d4efa487c827
nathangruber <grubernp@gmail.com>
2016-06-01 15:58:30 -0500

    edited div tags, tabbed code

--- includes/footer.php
+++ includes/footer.php
@@ -17,16 +17,17 @@
 <script src="assets/js/hidden.js"></script> 
 </body>
 </html>  -->
-<div class="righttext">
+
+  <footer id="footer" class="clearfix">
+  <div class="righttext">
                 Blake's Board Shop makes high quality, made in Wisconsin skateboard decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop. <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
                 </script>
             </div>
-<footer id="footer" class="clearfix">
       
  	<div id="footer-widgets">
 
-        <div class="container">
+     <div class="container">
 
         <div id="footer-wrapper">
 
@@ -34,50 +35,51 @@
             <div class="col-sm-6 col-md-2">
               <div id="meta-3" class="widget widgetFooter widget_meta">
               <ul>
-    		  <li><a href="index.php"><i class="fa fa-home fa-fw"></i> Home</a></li>
-			    <li><a href="about.php"><i class="fa fa-link"></i> About</a></li>
-			    <li><a href="sitemap.php"><i class="fa fa-envelope fa-fw"></i> Site Map</a></li>
+            		  <li><a href="index.php"><i class="fa fa-home fa-fw"></i> Home</a></li>
+        			    <li><a href="about.php"><i class="fa fa-link"></i> About</a></li>
+        			    <li><a href="sitemap.php"><i class="fa fa-envelope fa-fw"></i> Site Map</a></li>
              </ul>
              </div>     
-             </div> <!-- end widget1 -->
+            </div> <!-- end widget1 -->
 
             <div class="col-sm-6 col-md-2">
               		<div id="recent-posts-3" class="widget widgetFooter widget_recent_entries">
                     <ul>
-					<li>
-				     <a href="https://www.facebook.com/mr.dauod" target="_blank"><i class="fa fa-facebook"></i> Facebook</a>
-					</li>
-					<li>
-				      <a href="https://www.twitter.com/ayatquranCom" target="_blank"><i class="fa fa-twitter"></i> Twitter</a>
-					</li>
-					<li>
-				      <a href="http://www.youtube.com/user/sonsofye" target="_blank"><i class="fa fa-youtube"></i> Youtube</a>
-				   </li>
-  				</ul>
-  		</div>           
-   </div> <!-- end widget1 -->
-    <div class="col-sm-6 col-md-2">
+            					<li>
+            				     <a href="https://www.facebook.com/mr.dauod" target="_blank"><i class="fa fa-facebook"></i> Facebook</a>
+            					</li>
+            					<li>
+            				      <a href="https://www.twitter.com/ayatquranCom" target="_blank"><i class="fa fa-twitter"></i> Twitter</a>
+            					</li>
+            					<li>
+            				      <a href="http://www.youtube.com/user/sonsofye" target="_blank"><i class="fa fa-youtube"></i> Youtube</a>
+            				   </li>
+  				         </ul>
+  		           </div>           
+               </div> <!-- end widget1 -->
+           <div class="col-sm-6 col-md-2">
               <div id="meta-3" class="widget widgetFooter widget_meta">
               <ul>
-    		  <li><a href="register.php"><form action="register.php" method="post"><i class="glyphicon glyphicon-user" type="submit" value="Register"></i></i>Register</a></li>
-			    <li><a href="login.php"><form action="login.php" method="post"><i class="glyphicon glyphicon-cloud" type="submit" value="Login"></i></i>Login</a></li>
-			    <li><a href="logout.php"><form action="logout.php" method="post"><i class="glyphicon glyphicon-remove" type="submit" value="Logout"></i></i>Logout</a></li>
+            		  <li><a href="register.php"><form action="register.php" method="post"><i class="glyphicon glyphicon-user" type="submit" value="Register"></i></i>Register</a></li>
+        			    <li><a href="login.php"><form action="login.php" method="post"><i class="glyphicon glyphicon-cloud" type="submit" value="Login"></i></i>Login</a></li>
+        			    <li><a href="logout.php"><form action="logout.php" method="post"><i class="glyphicon glyphicon-remove" type="submit" value="Logout"></i></i>Logout</a></li>
               </ul>
              </div> <!-- end .row -->
-
-
-      <div id="sub-floor">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4 copyright">
-             &copy; 2016 Blake's Board Shop
             </div>
-            <div class="col-md-4 col-md-offset-4 attribution">
-             Developed by  <a target="_blank" href="#">Nathan Gruber</a>
+            <div id="sub-floor">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-4 copyright">
+                   &copy; 2016 Blake's Board Shop
+                  </div>
+                  <div class="col-md-4 col-md-offset-4 attribution">
+                   Developed by  <a target="_blank" href="#">Nathan Gruber</a>
+                  </div>
+                </div> <!-- end .row -->
+              </div>
             </div>
-          </div> <!-- end .row -->
-        </div>
-      </div>
+
+  
 
     </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript">
