a451cee63ae288d75e31864c50cdb612ceef2743
nathangruber <grubernp@gmail.com>
2016-06-01 15:54:36 -0500

    adjusting footer

--- includes/footer.php
+++ includes/footer.php
@@ -17,6 +17,11 @@
 <script src="assets/js/hidden.js"></script> 
 </body>
 </html>  -->
+<div class="righttext">
+                Blake's Board Shop makes high quality, made in Wisconsin skateboard decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop. <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+                </script>
+            </div>
 <footer id="footer" class="clearfix">
       
  	<div id="footer-widgets">
@@ -30,10 +35,11 @@
               <div id="meta-3" class="widget widgetFooter widget_meta">
               <ul>
     		  <li><a href="index.php"><i class="fa fa-home fa-fw"></i> Home</a></li>
-			  <li><a href="about.php"><i class="fa fa-link"></i> About</a></li>
-			  <li><a href="sitemap.php"><i class="fa fa-envelope fa-fw"></i> Site Map</a></li>
+			    <li><a href="about.php"><i class="fa fa-link"></i> About</a></li>
+			    <li><a href="sitemap.php"><i class="fa fa-envelope fa-fw"></i> Site Map</a></li>
              </ul>
-</div>      </div> <!-- end widget1 -->
+             </div>     
+             </div> <!-- end widget1 -->
 
             <div class="col-sm-6 col-md-2">
               		<div id="recent-posts-3" class="widget widgetFooter widget_recent_entries">
@@ -47,8 +53,9 @@
 					<li>
 				      <a href="http://www.youtube.com/user/sonsofye" target="_blank"><i class="fa fa-youtube"></i> Youtube</a>
 				   </li>
-				</ul>
-		</div>            </div> <!-- end widget1 -->
+  				</ul>
+  		</div>           
+   </div> <!-- end widget1 -->
     <div class="col-sm-6 col-md-2">
               <div id="meta-3" class="widget widgetFooter widget_meta">
               <ul>
@@ -57,12 +64,6 @@
 			    <li><a href="logout.php"><form action="logout.php" method="post"><i class="glyphicon glyphicon-remove" type="submit" value="Logout"></i></i>Logout</a></li>
               </ul>
              </div> <!-- end .row -->
-
-             <div class="righttext">
-                Blake's Board Shop makes high quality, made in Wisconsin skateboard decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop. <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-                </script>
-            </div>
 
 
       <div id="sub-floor">
