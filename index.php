cf7e2038828ee0244234202b53781bb83f074199
nathangruber <grubernp@gmail.com>
2016-05-31 21:58:34 -0500

    removed text

--- index.php
+++ index.php
@@ -38,9 +38,6 @@
             <div class="jumbotron">
   <h1>Blake's Board Shop</h1> 
   <br>
-  <p>Blake's Board Shop makes high quality, made in Wisconsin skateboard decks.<br> Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop. <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-                </script></p> 
 </div>
 <div class="container">
   <p></p> 
