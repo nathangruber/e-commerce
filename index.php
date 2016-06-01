2c9d0bb751cdac3ca86e6ebde272289c763eab2a
nathangruber <grubernp@gmail.com>
2016-05-31 21:34:29 -0500

    trying adjusted jumbotron

--- index.php
+++ index.php
@@ -36,9 +36,15 @@
     ?>
  
             <div class="jumbotron">
-            </div>
-            <div class="container">
-            </div>
+  <h1>Blake's Board Shop</h1> 
+  <p>Blake's Board Shop makes high quality, made in Wisconsin skateboard decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop. <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+                </script></p> 
+</div>
+<div class="container">
+  <p>This is some text.</p> 
+  <p>This is another text.</p> 
+</div>
     </header>
 
     <div class="container">
