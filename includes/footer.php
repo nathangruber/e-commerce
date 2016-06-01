54ff7e865ae47e3a965beaecf0dd4a18c58fa54d
nathangruber <grubernp@gmail.com>
2016-06-01 15:59:51 -0500

    adjusted class

--- includes/footer.php
+++ includes/footer.php
@@ -19,7 +19,7 @@
 </html>  -->
 
   <footer id="footer" class="clearfix">
-  <div class="righttext">
+  <div class="lefttext">
                 Blake's Board Shop makes high quality, made in Wisconsin skateboard decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop. <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
                 </script>
