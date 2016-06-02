a9e15efa4acdd88abd5b56437630ce5855b926df
nathangruber <grubernp@gmail.com>
2016-06-01 21:24:18 -0500

    changing nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -47,6 +47,10 @@ Database::disconnect();
    <!-- <li><a href="about.php" >About</a></li> -->
     <li><a href="contact.php">Contact Us</a></li>
     <li><a href="update.php">My Account</a></li>
+
+    <li><a href="update.php"></a></li>
+    <li><a href="update.php"></a></li>
+
     <li><a href="cart.php">Cart</a></li>
             </ul>
       <div class="container">
