08189b2b8b51543c0f894d670e225bc3cb6f3bc6
nathangruber <grubernp@gmail.com>
2016-06-01 22:03:12 -0500

    added twitter

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 
-<!--<div class="content"> <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
+<!--<div class="content"> --> <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-                </script></p></div> -->
+                </script></p></div> 
   <hr>
   <div class="footer">
   </div>
