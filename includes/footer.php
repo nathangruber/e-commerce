6a898a28fe08fb5016af675602cb56275e43cac5
nathangruber <grubernp@gmail.com>
2016-06-01 21:58:24 -0500

    added twitter

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
 
-<div class="content">Blake's Board Shop</div>
+<div class="content"> <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+                </script></p>  --></div>
   <div class="footer">
   </div>
   <center><p><small>&copy; 2016 Blake's Board Shop. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
