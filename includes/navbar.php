dc4d4b791c67b6aab2d64add850ef6edbbc924a0
nathangruber <grubernp@gmail.com>
2016-06-01 21:11:34 -0500

    added file path

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,7 +9,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.php"><img src="assets/images/wave.png" width="50"></a>     
+        <a class="navbar-brand" href="index.php"><img src="assets/images/bbs.png" width="50"></a>     
 
         </div>
          
