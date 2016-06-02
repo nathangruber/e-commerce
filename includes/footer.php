69acef9863cdfe5d62e959ea3327510c5f3a25f9
nathangruber <grubernp@gmail.com>
2016-06-01 21:50:21 -0500

    changing footer again

--- includes/footer.php
+++ includes/footer.php
@@ -1,34 +1,15 @@
 <hr>
-<table><tr>
-<td width="30%">
-<h2>About</h2>
+
+
+<div class="content">Blake's Board Shop</div>
+  <div class="footer">
+   <h2>About</h2>
 <p> Blake's Board Shop makes high quality, made in Wisconsin skateboard decks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop. <a href="https://twitter.com/bbshop" class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
                 </script></p>
-</td>
-<td width="10%"></td>
-<td width="33%">
-<h2>Navigation</h2>
-  <li><a href="##########">Link 1</a></li>
-  <li><a href="##########">Link 2</a></li>
-  <li><a href="##########">Link 3</a></li>
-  <li><a href="##########">Link 4</a></li>
-  <li><a href="##########">Link 5</a></li>
-</td>
-<td>
-<h2>Links</h2>
-  <li><a href="##########">Link 1</a></li>
-  <li><a href="##########">Link 2</a></li>
-  <li><a href="##########">Link 3</a></li>
-  <li><a href="##########">Link 4</a></li>
-  <li><a href="##########">Link 5</a></li>
-</td>
-</tr>
-</table>
-<br>
-<center><p><small>&copy; 2016 Blake's Board Shop. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
-<script src="assets/js/jquery.min.js" type="text/javascript">
-</script><script src="assets/js/bootstrap.min.js" type="text/javascript">
-</script>
-<script src="searchFunction.js"></script>
+  </div>
+  <center><p><small>&copy; 2016 hikeMe. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="searchFunction.js"></script>
 <script src="assets/js/hidden.js"></script> 
