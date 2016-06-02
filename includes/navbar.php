310671c15ad5dda6e8091fbcd815e1cd64ba2cd7
nathangruber <grubernp@gmail.com>
2016-06-01 21:11:04 -0500

    changing nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,25 +1,23 @@
 <nav class="navbar navbar-default navbar-fixed-top">
-
-  <div class="container">
-    <div class="navbar-header">
-
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
+        
+    <div class="container">
+        <div class="navbar-header">
+          
+        <button type="button" class="navbar-toggle btn-custom" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-    </button>
-      <!-- <a class="navbar-brand" href="index.php" id="black"><img src="assets/img/bbs.png" height="26" width="26"></a> -->
-        <a id="logo" class="navbar-brand" href="index.php"><img src="assets/img/bbs.png" height="36" width="36"></a>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php"><img src="assets/images/wave.png" width="50"></a>     
 
-     </div>
-
-
-   <div id="navbar" class="collapse navbar-collapse">
-
-    <ul class="nav navbar-nav">
-
-    <li><a href="index.php">Blake's Board Shop</a></li>
+        </div>
+         
+     <div id="navbar" class="collapse navbar-collapse">
+            
+        <ul class="nav navbar-nav">
+        
+        <li><a  href="index.php">Blake's Board Shop</a></li>    
 
 
     <?php
