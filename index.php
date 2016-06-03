928da9f93556eee74e9c2d290d1ce173f678e0ce
nathangruber <grubernp@gmail.com>
2016-06-02 22:25:05 -0500

    testing icons

--- index.php
+++ index.php
@@ -37,6 +37,68 @@
             <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/e-commerce/category.php?id=12" id="black"><img id="truck" src="assets/img/maxresdefault.jpg" alt="Skate Truck" height="187" width="333"></a>
         </div>
     </div> -->
+<div class="container-fluid">
+    <div class="row text-center">
+        <h3 style="color:white;font-family:verdana;">Snap Tight Image Tiles (Responisve)</h3>
+    </div>
+    <div class="row">
+        <div class="cover-card col-sm-4" style="background: url(http://lorempixel.com/300/200/nightlife/2) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-8" style="background: url(http://lorempixel.com/600/200/nightlife/3) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-3" style="background: url(http://lorempixel.com/300/200/nightlife/1) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-3" style="background: url(http://lorempixel.com/300/200/nightlife/4) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-3" style="background: url(http://lorempixel.com/300/200/nightlife/6) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-3" style="background: url(http://lorempixel.com/300/200/nightlife/) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+
+        <div class="cover-card col-sm-4" style="background: url(http://lorempixel.com/600/200/city/4) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-4" style="background: url(http://lorempixel.com/300/200/city/5) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-4" style="background: url(http://lorempixel.com/300/200/city/6) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-6" style="background: url(http://lorempixel.com/300/200/city/7) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+        <div class="cover-card col-sm-6" style="background: url(http://lorempixel.com/300/200/city/8) no-repeat center top;background-size:cover;">
+            <p>
+                Text Caption
+            </p>
+        </div>
+    </div>
 <?php require_once 'includes/footer.php'; ?>
 
 
