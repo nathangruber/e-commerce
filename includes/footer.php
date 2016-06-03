45fdff38cadabe4bd0bc2e63f2a34a5bca29663d
nathangruber <grubernp@gmail.com>
2016-06-02 22:09:23 -0500

    added css

--- includes/footer.php
+++ includes/footer.php
@@ -25,7 +25,7 @@
       <p class="navbar-text pull-left">&copy; 2016 Blake's Board Shop. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a> 
       </p>
       
-      <a href="https://twitter.com/bbshop"  class="twitter-follow-button pull-right" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
+      <a href="https://twitter.com/bbshop"  class="twitter-follow-button" data-show-count="false">Follow @bbshop</a> <script type="text/javascript">
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
                 </script></p>
     </div>
