dab39f707aa212011ea13ef48c33714b64d7c3da
nathangruber <grubernp@gmail.com>
2016-06-05 15:46:43 -0500

    only show cart when login

--- includes/navbar.php
+++ includes/navbar.php
@@ -47,7 +47,14 @@ Database::disconnect();
     <li><a href="about.php" >About</a></li>
     <li><a href="contact.php">Contact Us</a></li>
     <li><a href="update.php">My Account</a></li>
-    <li><a href="cart.php">Cart</a></li>
+    <?php
+	  if($logged==true){
+	  ?>
+	  <li><a href="cart.php">Cart</a></li>
+	  <?php
+	  }  
+	?>
+    
             </ul>
       <div class="container">
         <div class="col-md-3 pull-right">
