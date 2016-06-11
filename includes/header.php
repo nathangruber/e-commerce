c3b65649034a4b2b273abe0d0ebb9422146c37d3
nathangruber <grubernp@gmail.com>
2016-06-11 12:06:03 -0500

    corrected link paths

--- includes/header.php
+++ includes/header.php
@@ -10,9 +10,8 @@
 		<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
 		<link rel="stylesheet" type="text/css" href="search_style.css">
       
-      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-      <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-      <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/font-awesome.min.css">
       <link rel="stylesheet" href="assets/css/form-elements.css">
       <link rel="stylesheet" href="assets/css/style.css">
 
