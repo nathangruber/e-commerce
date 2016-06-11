891c1083b1259284f8c02a14e49e319f5023f85d
nathangruber <grubernp@gmail.com>
2016-06-11 16:19:26 -0500

    about edit

--- about.php
+++ about.php
@@ -18,21 +18,14 @@
         ?>
 <div class="container">
   <div class="jumbotron">
-    <h1><strong>YouTube Opening Example</strong></h1>
+    <h1><strong>About</strong></h1>
     <p class="lead">Example for "<a href="http://www.joshuawinn.com/opening-youtube-links-dynamically-in-a-twitter-bootstrap-modal-window/" target="_blank" title="View Post">Opening YouTube Links Dynamically in a Twitter Bootstrap Modal Window</a>" . Links to YouTube videos are opened automatically as an embedded video in a single modal window. No need for extra HTML or embed codes.</p>
-    
-    <!-- The HREF just points to a normal YouTube link -->
-    <p>
-      <a class="btn btn-primary btn-lg" href="http://www.youtube.com/watch?v=6w4FI0Jq0lI" role="button">Open a Video</a> 
-      
-      <a class="btn btn-primary btn-lg" href="http://www.youtube.com/watch?v=6w4FI0Jq0lI" role="button" data-width="640" data-height="360">Open Another Video</a>
-    </p>
-    
+
   </div>
 </div>
 
 
-    
+<?php require_once 'includes/footer.php'; ?>
 </body>
 </html>
                 <center><p>Blake's Board Shop makes high quality, made in Wisconsin skateboard and cruiserdecks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop.</p></center>
