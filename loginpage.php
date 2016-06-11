0a02fbffc405f2be3ec1004e5263335f5f562d1d
nathangruber <grubernp@gmail.com>
2016-06-11 12:24:20 -0500

    editing login page

--- loginpage.php
+++ loginpage.php
@@ -37,12 +37,6 @@
             <div class="inner-bg">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Bootstrap</strong> Login Form</h1>
-
-                        </div>
-                    </div>
-                    <div class="row">
                         <div class="col-sm-6 col-sm-offset-3 form-box">
                             <div class="form-top">
                                 <div class="form-top-left">
