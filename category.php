653dabae567a3efb6b4b0f5e498581946247c60b
nathangruber <grubernp@gmail.com>
2016-06-11 15:39:26 -0500

    margin edit

--- category.php
+++ category.php
@@ -6,7 +6,7 @@
 <body>
 <?php require_once 'includes/navbar.php'; ?>
 <div id="fullscreen_bg2" class="fullscreen_bg2"/>
-<div class="container" style="margin-top: 300px;">
+<div class="container" style="margin-top: 340px;">
       <div class="row">
         <table class="table table-bordered">
           <thead>
