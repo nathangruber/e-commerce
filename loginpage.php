9355f365f2f3d378c3d718310efa3b7c8b80e9a8
nathangruber <grubernp@gmail.com>
2016-06-11 13:03:59 -0500

    text edit

--- loginpage.php
+++ loginpage.php
@@ -11,28 +11,7 @@
 <body>
     <?php require_once 'includes/navbar.php';?>
     <div id="fullscreen_bg" class="fullscreen_bg"/>
-
-       <!--<div class="container">
-        <div class="starter-template">
-            <p class="lead">Enter Username and Password to login.<br></p>
-        </div>
-
-        <div>
-            <form action="includes/login.php" method="post">
-                <input type="text" name="username" placeholder="username"> <input type="password" name="password" placeholder="password"> <input class="btn btn-default" type="submit" value="Login">
-            </form>
-        </div>
-
-        <div>
-            <p>No Account? Register below</p>
-        </div>
-
-        <div>
-            <form action="register.php" method="post">
-                <input class="btn btn-default" type="submit" value="Register">
-            </form>
-        </div> -->
-         <div class="container">
+      <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-4 col-md-offset-4">
                 <h1 style="margin-top: 100px">Login</h1>
@@ -42,7 +21,7 @@
                   }  
                 ?>
                 <div class="starter-template">
-                    <p class="lead">Enter Username and Password to Login.<br></p>
+                    <p class="lead">Enter Username and Password<br></p>
                 </div>
         
                     <form class="form-horizontal" action="includes/login.php" method="post">
