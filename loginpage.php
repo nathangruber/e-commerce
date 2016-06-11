572de7c2ff5aa7f8f7662f6c58512c24d9f78223
nathangruber <grubernp@gmail.com>
2016-06-11 12:58:25 -0500

    adjusted columns / rows

--- loginpage.php
+++ loginpage.php
@@ -12,7 +12,7 @@
     <?php require_once 'includes/navbar.php';?>
     <div id="fullscreen_bg" class="fullscreen_bg"/>
 
-       <div class="container">
+       <!--<div class="container">
         <div class="starter-template">
             <p class="lead">Enter Username and Password to login.<br></p>
         </div>
@@ -31,7 +31,43 @@
             <form action="register.php" method="post">
                 <input class="btn btn-default" type="submit" value="Register">
             </form>
+        </div> -->
+         <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-4 col-md-offset-4">
+                <h1 style="margin-top: 100px">Login</h1>
+                <?php
+                  if($_GET['result']=='error'){
+                      echo '<b class="text-danger">Username or password not valid.</b>';
+                  }  
+                ?>
+                <div class="starter-template">
+                    <p class="lead">Enter Username and Password to Login.<br></p>
+                </div>
+        
+                    <form class="form-horizontal" action="includes/login.php" method="post">
+                      <div class="form-group">
+                        <label for="username">Username</label>
+                        <input name="username" id="username" type="text" class="form-control" placeholder="Username...">
+                      </div>
+                      
+                      <div class="form-group">
+                        <label for="name">Password</label>
+                        <input name="password" id="name" type="password" class="form-control" placeholder="Password...">
+                      </div>
+                      
+                      <div class="form-group">
+                      <button type="submit" class="btn btn-success">Login</button>
+                      </div>
+                    </form>
+                <div>
+                    <center><p>No Account? Register below</p></center>
+                    <center><a href="register.php" class="btn btn-default">Register</a></center>
+                    <br>
+                </div>
+            </div>
         </div>
+    </div>
     
 <?php require_once 'includes/footer.php';?>
 </body>
