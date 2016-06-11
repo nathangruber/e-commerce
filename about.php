d3610e5ee6af6984053a97faefcc25637874e612
nathangruber <grubernp@gmail.com>
2016-06-11 16:24:49 -0500

    added div tags

--- about.php
+++ about.php
@@ -16,12 +16,15 @@
             require_once'includes/navbar.php';
         }
         ?>
+
 <div class="container">
   <div class="jumbotron">
     <h1><strong>About</strong></h1>
   </div>
 </div>
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Blake's Board Shop makes high quality, made in Wisconsin skateboard and cruiserdecks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop.</p>
+<div>
+    <p>Blake's Board Shop makes high quality, made in Wisconsin skateboard and cruiserdecks. Our e-shop is committed to producing great decks at an affordbale price. Blake’s is a family-owned skateboard e-shop.</p>
+</div>
 <?php require_once 'includes/footer.php'; ?>
 </body>
 </html>
