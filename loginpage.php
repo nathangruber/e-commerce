8eeddce92de14cbe58128e85f929251eaa301c7b
nathangruber <grubernp@gmail.com>
2016-06-11 12:16:22 -0500

    adjusting fonts and buttons

--- loginpage.php
+++ loginpage.php
@@ -34,18 +34,12 @@
     </div>  --> 
     <!-- Top content -->
         <div class="top-content">
-            
             <div class="inner-bg">
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2 text">
                             <h1><strong>Bootstrap</strong> Login Form</h1>
-                            <div class="description">
-                                <p>
-                                    This is a free responsive login form made with Bootstrap. 
-                                    Download it on <a href="http://azmind.com"><strong>AZMIND</strong></a>, customize and use it as you like!
-                                </p>
-                            </div>
+
                         </div>
                     </div>
                     <div class="row">
