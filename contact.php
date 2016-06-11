1c7c51dbacb4676844c9ee1c6be6a166026aa1f1
nathangruber <grubernp@gmail.com>
2016-06-11 15:15:03 -0500

    removed offset

--- contact.php
+++ contact.php
@@ -57,7 +57,7 @@
                         </div>
                       </div>
                       <div class="form-group">
-                        <div class="col-sm-offset-12 col-sm-10">
+                        <div class="col-sm-12 col-sm-10">
                           <button type="submit" class="btn btn-success">Submit</button>
                         </div>
                       </div>
