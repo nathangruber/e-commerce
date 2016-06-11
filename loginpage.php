40306f79dd7b40883323914a0af4baf89850ed68
nathangruber <grubernp@gmail.com>
2016-06-11 12:45:31 -0500

    trying new login

--- loginpage.php
+++ loginpage.php
@@ -32,48 +32,19 @@
             </form>
         </div>
     </div>  --> 
-    <div class="container">
-    <div class="row">
-        <div class="col-md-4 col-md-offset-7">
-            <div class="panel panel-default">
-                <div class="panel-heading"> <strong class="">Login</strong>
+    <div id="fullscreen_bg" class="fullscreen_bg"/>
 
-                </div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form">
-                        <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
-                            <div class="col-sm-9">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email" required="">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword3" class="col-sm-3 control-label">Password</label>
-                            <div class="col-sm-9">
-                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password" required="">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-9">
-                                <div class="checkbox">
-                                    <label class="">
-                                        <input type="checkbox" class="">Remember me</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group last">
-                            <div class="col-sm-offset-3 col-sm-9">
-                                <button type="submit" class="btn btn-success btn-sm">Sign in</button>
-                                <button type="reset" class="btn btn-default btn-sm">Reset</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="panel-footer">Not Registered? <a href="#" class="">Register here</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container">
+
+    <form class="form-signin">
+        <h1 class="form-signin-heading text-muted">Sign In</h1>
+        <input type="text" class="form-control" placeholder="Email address" required="" autofocus="">
+        <input type="password" class="form-control" placeholder="Password" required="">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">
+            Sign In
+        </button>
+    </form>
+
 </div>
 <?php require_once 'includes/footer.php';?>
 </body>
