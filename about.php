aa575f346d6111585e46b4bfbc375ba4c4b9836f
nathangruber <grubernp@gmail.com>
2016-06-11 21:31:06 -0500

    removed code

--- about.php
+++ about.php
@@ -31,28 +31,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 text-center">
-                    <div class="box">
-                        <div class="box-content">
-                            <h1 class="tag-title">Heading</h1>
-                            <hr />
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pharetra quam sollicitudin nibh aliquam finibus. Etiam efficitur felis vel imperdiet varius. Maecenas bibendum elementum molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus finibus semper. Fusce molestie tincidunt leo vel varius. Nam scelerisque nulla feugiat leo consequat, id dignissim sem tincidunt. Proin elit mauris, hendrerit in varius sed, facilisis sit amet neque.</p>
-                            <br />
-                            <a href="ppc.html" class="btn btn-block btn-primary">Learn more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 text-center">
-                    <div class="box">
-                        <div class="box-content">
-                            <h1 class="tag-title">Heading</h1>
-                            <hr />
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pharetra quam sollicitudin nibh aliquam finibus. Etiam efficitur felis vel imperdiet varius. Maecenas bibendum elementum molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus finibus semper. Fusce molestie tincidunt leo vel varius. Nam scelerisque nulla feugiat leo consequat, id dignissim sem tincidunt. Proin elit mauris, hendrerit in varius sed, facilisis sit amet neque.</p>
-                            <br />
-                            <a href="ppc.html" class="btn btn-block btn-primary">Learn more</a>
-                        </div>
-                    </div>
-                </div>
+               
+               
             </div>           
         </div>
     </div>
